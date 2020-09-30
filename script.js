61dec630c7acb6c5411c0b4eb1618dfb15188329
KHallam <kjhallam321@gmail.com>
2020-09-29 22:05:57 -0500

    cleaned up variables and removed console logs from js file

--- script.js
+++ script.js
@@ -1,168 +1,45 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
 
-var lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberCase = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbolCase = ["!",  "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[",  "]", "^", "`", "{", "|", "}", "~"];
 
-var symbolCase = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "]",
-  "^",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-];
 var passCombo = [];
 var passCode = "";
 
 function generatePassword() {
   
-  // prompt for the length of the password
-  var passwordLength = parseInt(window.prompt("How long would you like your password to be?"));
-    while(isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8);
-    passwordLength === false;
-    if (passwordLength > 8 && passwordLength < 128){
-    alert("Is a valid number!");
-}
-
-  // if (passwordLength < 8 || passwordLength > 128) {
-  //   alert("Invalid number entered");
-  //   passwordLength === true;
-  // } else if (passwordLength > 8 && passwordLength < 128){
-  //   parseInt(prompt(" ("+ passwordLength +") is a valid number", ""));
-  // }
-  // else { (isNaN(passwordLength))
-  //   parseInt(prompt("This is not a number. Please enter an valid number between 8 and 128", ""));
-  // } 
-
+ var passwordLength = (prompt("How long would you like your password to be?"));
+    if (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8) {
+    var passwordLength = (prompt("Invalid number, please enter a number between 8 and 128"));
+        
+    } alert("Your password will contain (" +passwordLength+") characters.");
+ 
   var useLowercase = confirm("Would you like to use lowercase letters?");
-  console.log(useLowercase);
 
   var useUppercase = confirm("Would you like to use uppercase letters?");
-  console.log(useUppercase);
-
-  var useNumber = confirm("Would you like to use number?");
-  console.log(useNumber);
-
-  var useSymbol = confirm("Would you like to use special character?");
-  console.log(useSymbol);
-
-  if (useLowercase === true) {
-    passCombo = passCombo.concat(lowerCase);
-  }
-  console.log(passCombo);
-
-  if (useUppercase === true) {
-    passCombo = passCombo.concat(upperCase);
-  }
-  console.log(passCombo);
-
-  if (useNumber === true) {
-    passCombo = passCombo.concat(numberCase);
-  }
-  console.log(passCombo);
-
-  if (useSymbol === true) {
-    passCombo = passCombo.concat(symbolCase);
-  }
-  else if
-  (useLowercase === false && useUppercase === false && useNumber === false && useSymbol === false) {
-    alert("You must have at least 1 character option selected!");
- }
-
   
-  for (var i = 0; i < passwordLength; i++) {
-    passCode = passCode + passCombo[Math.floor(Math.random() * passCombo.length)];
+  var useNumber = confirm("Would you like to use number?");
+ 
+  var useSymbol = confirm("Would you like to use special character?");
+  
+  if (useLowercase === false && useUppercase === false && useNumber === false && useSymbol === false) {
+    alert("You must have at least 1 character option selected!");
+ 
+  } else {
+    if (useLowercase === true) {passCombo = passCombo.concat(lowerCase);}
+    if (useUppercase === true) {passCombo = passCombo.concat(upperCase);}
+    if (useNumber === true) {passCombo = passCombo.concat(numberCase);}
+    if (useSymbol === true) {passCombo = passCombo.concat(symbolCase);}
+ 
+   for (var i = 0; i < passwordLength; i++) {
+    passCode = passCode + passCombo[Math.floor(Math.random() * passCombo.length)];}
   }
-  console.log(passCode);
-
-  //  be careful with the type it receives --> string , you may want to use a number hint hint (parseInt(string))
-
-  return (passCode);
+  return passCode; 
 }
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -170,5 +47,4 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
